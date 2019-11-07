c38b05376376195e2f1906226f16a385d005d796
Diane de Lassence <dianedelassence@gmail.com>
2019-11-07 09:25:25 +0100

    json property

--- proprioo/src/js/property.js
+++ proprioo/src/js/property.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../index.css';
 
+let obj = JSON.parse('{"id":"96890eb6-8689-5e05-9699-15cfc4c8896b","type":"house","rooms":2,"surface":44,"price":285645,"cave":false,"garden":627}')
+
+
 class Property extends React.Component {
     constructor(props) {
         super(props);
@@ -19,13 +22,14 @@ class Property extends React.Component {
         return (
             <div className="property">
             <h1>Caractéristique du bien : </h1>
-            <p>Type : </p>
-            <p>Nombre de pièces : </p>
-            <p>Surface en m2 : </p>
-            <p>Prix : </p>
-            <p>Cave : </p>
-            <p>Jardin en m2 : </p>
+            <p>Type : {(obj.type)}</p>
+            <p>Nombre de pièces : {(obj.rooms)}</p>
+            <p>Surface en m2 : {(obj.surface)}</p>
+            <p>Prix : {(obj.price)}</p>
+            <p>Cave : {(obj.cave)}</p>
+            <p>Jardin en m2 : {(obj.garden)}</p>
             <button onClick={this.handleClick}>{this.state.isToggleOn ? 'Liste des acheteurs potentiels' : 'Afficher les acheteurs potentiels'}</button>
+            {/* <button onClick={() => { }}>Résultats</button> */}
             </div>
         );
         console.log(this.state.isToggleOn);
